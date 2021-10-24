2a2d916a56a07892a8eb0ea3cdd04ca648be7d18
Mukul Jain <mukuljain422@gmail.com>
2021-10-24 16:57:27 +0530

    Created stack

--- src/com/stacknqueue/StackAndQueueMain.java
+++ src/com/stacknqueue/StackAndQueueMain.java
@@ -1,7 +1,15 @@
 package com.stacknqueue;
 
+
 public class StackAndQueueMain {
 	public static void main(String args[]) {
 		System.out.println("Welcome To Stack and Queue Problem");
+		
+		System.out.println("\n===================");
+		
+		System.out.println("Create Stack");
+		StackOperation.addElement();
+		System.out.println("===================");
+		
 	}
 }
